b0bbbf119e973595b7a21edab58b457d70e8dc22
hellsontime <roma67177@gmail.com>
2022-08-09 16:06:16 +0300

    remove SupportService service

--- module/SupportApi/src/V1/Rest/SupportService/SupportServiceCollection.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace SupportApi\V1\Rest\SupportService;
-
-use Laminas\Paginator\Paginator;
-
-class SupportServiceCollection extends Paginator
-{
-}
--- module/SupportApi/src/V1/Rest/SupportService/SupportServiceEntity.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace SupportApi\V1\Rest\SupportService;
-
-class SupportServiceEntity
-{
-}
