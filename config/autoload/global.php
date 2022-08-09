0d48cbf83a6067322e07a40d3bb526df864b5bd6
hellsontime <roma67177@gmail.com>
2022-08-09 18:26:39 +0300

    add new basic_http_auth auth method

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,11 +4,23 @@ return [
         'selectors' => [],
     ],
     'db' => [
-        'driver' => 'Pdo',
-        'dsn'    => 'mysql:dbname=laminas;host=localhost;user=root;password=root;charset=utf8mb4',
+        'driver' => \Pdo::class,
+        'dsn' => 'mysql:dbname=laminas;host=localhost;user=root;password=root;charset=utf8mb4',
     ],
     'api-tools-mvc-auth' => [
         'authentication' => [
+            'adapters' => [
+                'basic_http_auth' => [
+                    'adapter' => \Laminas\ApiTools\MvcAuth\Authentication\HttpAdapter::class,
+                    'options' => [
+                        'accept_schemes' => [
+                            0 => 'basic',
+                        ],
+                        'realm' => 'api',
+                        'htpasswd' => 'data/htpasswd',
+                    ],
+                ],
+            ],
             'map' => [
                 'SupportApi\\V1' => 'basic_http_auth',
             ],
